e86b4d4a93c7626d521b7e72bde755ff1aad90f8
Matt Frankmore <m.frankmore@outlook.com>
2020-04-22 15:53:07 -0600

    Added AddonDisplay, No css yet, needs testing

---
+++ client/src/components/AddonDisplay.vue
@@ -0,0 +1,40 @@
+<template>
+    <main class="addon_display">
+        <h1 class="addon_display_header">Addon Buyers</h1>
+        <div class="table_view">
+        <table class="addon_display_table">
+            <thead>
+                <tr>
+                    <th class="table_c1"></th>
+                    <th class="table_c2"></th>
+                    <th class="table_c3"></th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr class="addon_table_rows">
+                    <td>{{buyers[b].name }}</td>
+                </tr>
+            </tbody>
+        </table>
+        </div>
+    </main>
+</template>
+
+<script>
+    import{mapState, mapActions} from 'vuex'
+    export default {
+        data() {
+            return{
+                addonDisplaySpeed: 15000,   // Needs Testing
+                buyers: []
+            }
+        },
+        created: function() {
+            setInterval(() => this.addonDisplaySpeed)
+            this.fetchSaleNumber()
+        }
+    }
+</script>
+
+<css>
+</css>
